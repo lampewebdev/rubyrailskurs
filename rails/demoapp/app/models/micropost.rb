f59d9b348e70e286f34fb04c9c39821da9c0f4be
Michael Lampe <mgoldhand@googlemail.com>
2012-08-15 23:05:07 +0200

    demoappfertig

---
+++ rails/demoapp/app/models/micropost.rb
@@ -0,0 +1,3 @@
+class Micropost < ActiveRecord::Base
+  attr_accessible :content, :user_id
+end
--- rails/demoapp/config/routes.rb
+++ rails/demoapp/config/routes.rb
@@ -1,4 +1,8 @@
 Demoapp::Application.routes.draw do
+  get "welcome/index"
+
+  resources :microposts
+
   resources :users
 
   # The priority is based upon order of creation:
@@ -50,7 +54,7 @@ Demoapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
